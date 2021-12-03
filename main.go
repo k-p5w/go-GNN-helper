a331fd1d17257d1f4902b71c7c38da36007ab95d
k-p5w <kos.p5w@gmail.com>
2021-12-03 15:01:17 +0900

    たのしくなってきた

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	table2svg "github.com/k-p5w/go-table2svg/api"
+)
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9999"
+	}
 	fmt.Println("hey")
+	http.HandleFunc("/", table2svg.Handler)
+	http.ListenAndServe(":"+port, nil)
 }
